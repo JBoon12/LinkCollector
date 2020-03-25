9480bd382322a209aa3cb640cc95951083575fb0
jinseok2.hwang <jinseok2.hwang@lge.com>
2020-03-25 16:16:47 +0900

    Initial Commit

---
+++ LinkCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkCollector
+//
+//  Created by Jinseok Hwang on 2020/03/25.
+//  Copyright © 2020 Jinseok Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
